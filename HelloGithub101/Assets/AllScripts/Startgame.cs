f7836f794d7748f045530d0cea0645d111a7ef52
Harshit Singh <56515247+1208hrsht@users.noreply.github.com>
2020-10-02 01:45:32 +0530

    Update Startgame.cs

--- HelloGithub101/Assets/AllScripts/Startgame.cs
+++ HelloGithub101/Assets/AllScripts/Startgame.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//Code for changing levels
 public class Startgame : MonoBehaviour
 {
     public void starto()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //'+1' Increases the level of the game
     }
 }
